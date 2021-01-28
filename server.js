67fb3a56558f2a03a4cd1a18c55013c6eca4c65f
manvendra-rajpoot <manvendra141986@gmail.com>
2021-01-28 14:49:10 +0530

    adding login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express(); //craete an instance
+
+app.use(bodyParser.json());
 
 const mockUserData=[
     {name:'Mark'},
@@ -21,6 +24,29 @@ app.get('/users/:id',function(req,res){
 		message: 'got one user',
 		user: req.params.id
 	})
+});
+
+app.post('/login',function(req,res){
+    //password is encrypted using 'bcrypt' before sending to db
+    const username=req.body.username;
+    const password=req.body.password;
+
+    //this is from db
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
 });
 
 app.listen(8000,function(){
